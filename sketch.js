8a7a5b202096c4ca90bb3748bc509448e0774f6a
kennedy590 <kennedymarins2902008@gmail.com>
2024-06-20 14:58:46 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup () {
+    createCanvas(600, 600);
+    background("black");
+
+  }
+  function draw() {
+    stroke ("blue");
+    Fill("red");
+
+    if(mouseIsPressed) 
+      rect(MouseX, mouseY, 20, 35,);
+     
+    { 
+
+ }
